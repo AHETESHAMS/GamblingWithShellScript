48ef0a1d14d4703033cf44d84ca38fc702f69b32
AHETESHAMS <aheteshams007@gmail.com>
2020-01-17 15:24:35 +0530

    Added: Gambler Either Win or Loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -5,6 +5,11 @@ i=0
 while [[ $i -lt 10 ]]
 do
 randomCheck=$(( RANDOM%2 ))
-echo $rarandomCheck
+if [[ $randomCheck -eq 0 ]]
+then
+	echo "Player Won..."
+else
+	echo "Player Loose..."
+fi
 (( i++ ))
 done
