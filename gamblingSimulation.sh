e6a117d8f779af6355421988f029a5c70be9392b
AHETESHAMS <aheteshams007@gmail.com>
2020-01-17 15:53:32 +0530

    Added:  Increament and Decrement of Stock acoording to win or Loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -x
 cash=100
-bet=1
+bet=20
+goal=200
 i=0
-while [[ $i -lt 10 ]]
+while [[ $cash -gt 20 && $i -lt 10 ]]
 do
 randomCheck=$(( RANDOM%2 ))
-if [[ $randomCheck -eq 0 ]]
+if [[ $randomCheck -eq 1 ]]
+then
+	cash=$(( cash+bet*2 ))
+else
+	cash=$(( cash-bet*2 ))
+fi
+(( i++ ))
+done
+if [[ $cash -ge $goal ]]
 then
 	echo "Player Won..."
 else
 	echo "Player Loose..."
 fi
-(( i++ ))
-done
